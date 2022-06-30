d31338563afe92d80eeee5b6fb2903b928e90390
shaAvinash0608 <103634746+shaAvinash0608@users.noreply.github.com>
2022-06-30 23:06:12 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,11 @@
-import React from 'react'
-const Post = ({ele}) => {
-    return (
-        <div className="post">
-        <h3>{ele.title}</h3>
-         <p>{ele.body}</p>
-
-        </div>
-    );
+import React from "react";
+const Post = ({ ele }) => {
+  return (
+    <div className="post">
+      <h3>{ele.title}</h3>
+      <p>{ele.body}</p>
+    </div>
+  );
 };
 
-export { Post }
+export { Post };
